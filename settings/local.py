9e7caa8e76a24fc8f6ca3cdd09ffe5580277f2d3
Jack <jackhancock23@gmail.com>
2013-06-14 20:04:14 +0000

    update ^X^O

---
+++ settings/local.py
@@ -0,0 +1,18 @@
+# Microblog local settings.
+
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'microblog',                      
+        'USER': '',                      
+        'PASSWORD': '',                  
+        'HOST': '',                      
+        'PORT': '',                      
+    }
+}
